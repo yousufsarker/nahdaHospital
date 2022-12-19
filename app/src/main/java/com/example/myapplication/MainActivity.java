176a9e180a3acee157104e107a58eaa93227e731
usuf git <ysarkar31@gmail.com>
2022-12-19 16:46:24 +0400

    this is sample project for vcs

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toast.makeText(this, "Welcome to Lab",Toast.LENGTH_LONG).show();
+    }
+}
